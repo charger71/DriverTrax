d519dfe6aba186a2ef187235188777ec0d6d9966
Don Hansen <dhansen@area71.com>
2026-05-23 06:19:52 -0400

    steering wheel icon animation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // DriverTrax Service Worker
 // Provides offline support and caches app assets
-const CACHE_VERSION = "drivertrax-v3.1";
+const CACHE_VERSION = "drivertrax-v3.2";
 const APP_SHELL = [
   "./",
   "./index.html",

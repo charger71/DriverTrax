148afdd1c9bbc8cf02696be262f601a87930a482
Don Hansen <dhansen@area71.com>
2026-05-23 06:12:26 -0400

    logo update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // DriverTrax Service Worker
 // Provides offline support and caches app assets
-const CACHE_VERSION = "drivertrax-v3.0";
+const CACHE_VERSION = "drivertrax-v3.1";
 const APP_SHELL = [
   "./",
   "./index.html",

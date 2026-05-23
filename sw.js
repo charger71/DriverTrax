36658a865bd3077e27c51f1a669ee83faa499d7b
Don Hansen <dhansen@area71.com>
2026-05-23 10:48:31 -0400

    sdk issues

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // DriverTrax Service Worker
 // Provides offline support and caches app assets
-const CACHE_VERSION = "drivertrax-v3.8";
+const CACHE_VERSION = "drivertrax-v3.9";
 const APP_SHELL = [
   "./",
   "./index.html",

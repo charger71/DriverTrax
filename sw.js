6037025f62b2e6ccc928de73c89e231ab4c2117c
Don Hansen <dhansen@area71.com>
2026-06-30 14:44:52 -0400

    multiple updates x4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // DriverTrax Service Worker
 // Provides offline support and caches app assets
-const CACHE_VERSION = "drivertrax-v7.6-vin-keypad-safe-area";
+const CACHE_VERSION = "drivertrax-v7.7-scanner-safe-area";
 const APP_SHELL = [
   "./",
   "./index.html",
